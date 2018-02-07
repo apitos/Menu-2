403667da4c478f3659f90b2d793bd42403482aed
Stewart ilondanga <stilondanga@gmail.com>
2018-02-07 15:07:41 +0300

    add app/src/main/java/io/github/stewilondanga/menu/MainActivity.java

--- app/src/main/java/io/github/stewilondanga/menu/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/menu/MainActivity.java
@@ -11,4 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void printToLogs(View view) {
+        // Find first menu item TextView and print the text to the logs
+
+        // Find second menu item TextView and print the text to the logs
+
+        // Find third menu item TextView and print the text to the logs
+
+    }
 }
